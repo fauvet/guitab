c8cbfda604249e4746da0752947f3d6a753a11eb
Guillaume FAUVET <g.fauvet.pers@gmailcom>
2024-04-22 12:42:05 +0200

    Update component-loader to allow sub-components

--- component-loader.js
+++ component-loader.js
@@ -1,32 +1,39 @@
-function extractScriptContents(componentContent) {
-  const regex = /<script.*?>([\s\S]*?)<\/script>/gi;
-  const scripts = [];
-  let match;
+(async () => {
+  function extractScriptContents(componentContent) {
+    const regex = /<script.*?>([\s\S]*?)<\/script>/gi;
+    const scripts = [];
+    let match;
 
-  while ((match = regex.exec(componentContent)) !== null) {
-    scripts.push(match[1]);
-  }
-
-  return scripts;
-}
-
-document.addEventListener("DOMContentLoaded", async function () {
-  const components = document.getElementsByTagName("component");
-
-  for (const component of components) {
-    const htmlFilePath = component.attributes.src.value;
-    const response = await fetch(htmlFilePath);
-    const componentContent = await response.text();
-    component.insertAdjacentHTML("afterend", componentContent);
-
-    const scriptContents = extractScriptContents(componentContent);
-
-    for (const scriptContent of scriptContents) {
-      const script = document.createElement("script");
-      script.textContent = scriptContent;
-      document.head.appendChild(script);
+    while ((match = regex.exec(componentContent)) !== null) {
+      scripts.push(match[1]);
     }
 
-    component.remove();
+    return scripts;
   }
-});
+
+  async function injectComponents() {
+    const components = document.getElementsByTagName("component");
+    if (!components.length) return;
+
+    for (const component of components) {
+      const htmlFilePath = component.attributes.src.value;
+      const response = await fetch(htmlFilePath);
+      const componentContent = await response.text();
+      component.insertAdjacentHTML("afterend", componentContent);
+
+      const scriptContents = extractScriptContents(componentContent);
+
+      for (const scriptContent of scriptContents) {
+        const script = document.createElement("script");
+        script.textContent = scriptContent;
+        document.head.appendChild(script);
+      }
+
+      component.remove();
+    }
+
+    injectComponents();
+  }
+
+  document.addEventListener("DOMContentLoaded", injectComponents);
+})();
